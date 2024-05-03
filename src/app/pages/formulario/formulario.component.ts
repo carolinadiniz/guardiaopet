fe3a611a7a9ccebc3b34de82871e3cadc65d1fea
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2024-05-03 07:12:07 -0300

    adiciona função para o formulário

--- src/app/pages/formulario/formulario.component.ts
+++ src/app/pages/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  formulario = new FormGroup({
+    fotos: new FormControl(''),
+    nome: new FormControl(''),
+    idade: new FormControl(''),
+    peso: new FormControl(''),
+    especie: new FormControl(''),
+    porte: new FormControl(''),
+    descricao: new FormControl(''),
+    tags: new FormControl(''),
+    estado: new FormControl(''),
+    cidade: new FormControl(''),
+    email: new FormControl(''),
+    telefone: new FormControl(''),
+  })
 
+  onSubmit(): void {
+    console.log(this.formulario.value)
+  }
 }
