e3e41a1fd6d2697f3ee21c33b83b2ed255deebd6
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2024-05-03 07:14:15 -0300

    adiciona rotas para home/anima/formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioComponent } from './pages/formulario/formulario.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AnimalComponent } from './pages/animal/animal.component';
 
 const routes: Routes = [
-  
+  {path: '', redirectTo: '/home', pathMatch:'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'animal', component: AnimalComponent},
+  { path: 'formulario', component: FormularioComponent }
 ];
 
 @NgModule({
