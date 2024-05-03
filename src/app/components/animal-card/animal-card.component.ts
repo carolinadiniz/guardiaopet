fb54507a5f1e7b4b2bc11593648e29b7d4a31332
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2024-05-03 07:13:12 -0300

    cria variável mocap
    cria uma variável mocap para criar a estrutura do template usando a variável

--- src/app/components/animal-card/animal-card.component.ts
+++ src/app/components/animal-card/animal-card.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './animal-card.component.html',
   styleUrl: './animal-card.component.css'
 })
-export class AnimalCardComponent {
+export class AnimalCardComponent{
+  data = {
+    "id": 0,
+    "id-doador": 0,
+    "nome": "Doggo",
+    "imageUrl": "../../../assets/images/animal1.jpeg",
+    "localizacao": ["RJ", "Magé"],
+    "peso": 15,
+    "idade":2,
+    "especie": "Cachorro",
+    "porte": "Grande",
+    "descricao": "Doggo foi resgatado na rua, estava magrinho. Demos um bom banho, antipulga e agora está em lar temporário aguardando adoção. \n \n Interessados precisam enviar um vídeo da casa, com o local onde vai dormir e o portão de acesso à rua. Após isso, entrevistamos.",
+    "tags": [],
+    "email": "exemplo@gmail.com",
+    "telefone": "(21) 98765-4321"
+  }
 
 }
